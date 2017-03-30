27bb9bd3ae184f0964fa492c96117da461fa86f3
oguzgelal <o.gelal77@gmail.com>
2017-03-30 11:35:53 +0200

    fixes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,16 +8,31 @@ angular.module('semavisApp').controller('MainCtrl', function ($rootScope, $scope
     corpus: ''
   };
 
+  $scope.output = {
+    relatedKeywords: [],
+    relatedKeywordsHighlightRegex: null
+  };
+
   $scope.analyze = function () {
     $scope.textEditActive = false;
     $rootScope.loading = true;
     api.extractKeywords($scope.input.corpus).then(function (res) {
-      var related = res.data.join(', ');
-      swal('Extracted keywords', related, 'success');
+      $scope.output.relatedKeywords = res.data;
+      $scope.highlightRegex();
+      //var related = res.data.join(', ');
+      //swal('Extracted keywords', related, 'success');
       $rootScope.loading = false;
     }, function () {
       swal('Oops...', 'Something went wrong!', 'error');
       $rootScope.loading = false;
+    });
+  };
+
+  $scope.highlightRegex = function () {
+    var regKeys = $scope.output.relatedKeywords.join('|');
+    $scope.output.relatedKeywordsHighlightRegex = new RegExp('\\b(?:' + regKeys + ')\\b', 'gi');
+    $scope.input.corpus = $scope.input.corpus.replace($scope.output.relatedKeywordsHighlightRegex, function (txt) {
+      return '<span class="highlight">' + txt + '</span>';
     });
   };
 
