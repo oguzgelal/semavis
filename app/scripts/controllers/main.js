56557de34ddacd3dcfe8431526631fe7b575ed1f
oguzgelal <o.gelal77@gmail.com>
2017-03-31 15:30:59 +0200

    changes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,32 +1,38 @@
 'use strict';
 
-angular.module('semavisApp').controller('MainCtrl', function ($rootScope, $scope, api) {
+angular.module('semavisApp').controller('MainCtrl', function ($rootScope, $scope, $timeout, $compile, api) {
 
   $scope.views = {
     'text': {
       closed: false,
       minimized: false,
+      maximized: false,
       edit: true
     },
     'heatmap': {
       closed: false,
-      minimized: false
+      minimized: false,
+      maximized: false
     },
     'details': {
       closed: false,
-      minimized: false
+      minimized: false,
+      maximized: false
     },
     'relevant': {
       closed: false,
-      minimized: false
+      minimized: false,
+      maximized: false
     },
     'suggestions': {
       closed: false,
-      minimized: false
+      minimized: false,
+      maximized: false
     },
     'readability': {
       closed: false,
-      minimized: false
+      minimized: false,
+      maximized: false
     }
   };
 
@@ -39,6 +45,34 @@ angular.module('semavisApp').controller('MainCtrl', function ($rootScope, $scope
     relatedKeywords: [],
     relatedKeywordsHighlightRegex: null
   };
+
+  $scope.closeView = function (view) { $scope.views[view].closed = true; };
+  $scope.openView = function (view) { $scope.views[view].closed = false; };
+  $scope.minimizeView = function (view) { $scope.views[view].minimized = !$scope.views[view].minimized; };
+  $scope.maximizeView = function (view) {
+    // TODO: find a better way
+    $scope.views[view].maximized = !$scope.views[view].maximized;
+    $rootScope.viewMaximized = $scope.views[view].maximized;
+    $timeout(function () {
+      if ($rootScope.viewMaximized) {
+        var el = document.getElementById(view).cloneNode(true);
+        el.id = 'remove-view';
+        var parent = document.getElementById('section-copy');
+        if (parent) {
+          parent.innerHTML = '';
+          parent.appendChild(el);
+          $compile(el)($scope);
+        }
+      }
+      else {
+        var removeEl = document.getElementById('remove-view');
+        if (removeEl) { document.removeChild(removeEl); }
+      }
+    });
+  };
+  $scope.isMinimized = function (view) { return $scope.views[view].minimized; };
+  $scope.isMaximized = function (view) { return $scope.views[view].maximized; };
+  $scope.isClosed = function (view) { return $scope.views[view].minimized; };
 
   $scope.toggleEdit = function (editOn) {
     if (editOn) {
