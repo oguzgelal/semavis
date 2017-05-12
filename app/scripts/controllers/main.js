0271764374fed6131b65dfb95423c1d3b569da46
oguzgelal <o.gelal77@gmail.com>
2017-05-12 02:12:09 +0200

    better clusters - solid suggestions

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,8 +4,8 @@ angular.module('semavisApp').controller('MainCtrl', function ($rootScope, $scope
 
   $scope.heatmapSize = 128; // 128 x 128
   $scope.heatmapDimentions = 300; // 300px
-  $scope.clusterSize = 10;
-  $scope.clusterQuantity = 300;
+  $scope.clusterSize = 6;
+  $scope.clusterQuantity = 500;
 
   $scope.columns = {
     'left': ['text'],
@@ -66,7 +66,8 @@ angular.module('semavisApp').controller('MainCtrl', function ($rootScope, $scope
       suggestedKeywords: [],
       fingerprints: [],
       heatmap: [],
-      clusterMarks: []
+      clusterMarks: [],
+      keywords: []
     };
 
     $scope.heatmapMax = 0;
@@ -79,11 +80,86 @@ angular.module('semavisApp').controller('MainCtrl', function ($rootScope, $scope
         $scope.output.heatmap[i][y] = 0;
       }
     }
+
+    $scope.initResetSuggestions();
+  };
+
+  $scope.initResetSuggestions = function () {
+    $scope.suggestionClickedStart = '';
+    $scope.suggestionClickedEnd = '';
+    $scope.suggestionMentionLess = [];
+    $scope.suggestionMentionMore = [];
+    $scope.suggestionMentionMoreContext = [];
   };
 
   $scope.setColumnLocation = function (loc) {
     $scope.views.columnLocation = loc;
     $scope.saveLs();
+  };
+
+  $scope.helpClicked = function () {
+    var txt = 'The heatmap above is a semantic visualisation of your content. It is very simple to analyze and make your content better and more relevant. When you have smaller shaded areas dispersed all over the heatmap, that means you areas mentioning a lot of things, and you should keep more in the boundaries of your focus topic. The bigger the shaded areas are, and the closer they are to eachother, the better and more relevant your content will be. This tool will help you merge all the shaded areas into one to create the top-notch content.';
+    swal('Analyzing the Heatmap', txt, 'info');
+  };
+
+  $scope.mapClicked = function (density, row, col) {
+    if (!$scope.suggestionClickedStart) {
+      if (!density) { swal('', 'This group of keywords are out of the scope of your content. Please choose from a cluster or a denser area.', 'warning'); }
+      else { $scope.suggestionClickedStart = row + '-' + col; }
+    }
+    else if (!$scope.suggestionClickedEnd) {
+      if (!density) { swal('', 'This group of keywords are out of the scope of your content. Please choose from a cluster or a denser area.', 'warning'); }
+      else {
+        $scope.suggestionClickedEnd = row + '-' + col;
+        $scope.suggestionAnalysis();
+      }
+    }
+    else { $scope.initResetSuggestions(); }
+  };
+
+  $scope.findClusterKeywords = function (row, col) {
+    var clusterKeys = [];
+    var clusterKeywordSize = 30;
+    var clusterStartRow = row - (clusterKeywordSize / 2);
+    var clusterStartCol = col - (clusterKeywordSize / 2);
+    if (clusterStartRow < 0) { clusterStartRow = 0; }
+    if (clusterStartCol < 0) { clusterStartCol = 0; }
+    var clusterEndRow = row + (clusterKeywordSize / 2);
+    var clusterEndCol = col + (clusterKeywordSize / 2);
+    if (clusterEndRow >= $scope.heatmapSize) { clusterEndRow = $scope.heatmapSize - 1; }
+    if (clusterEndCol >= $scope.heatmapSize) { clusterEndCol = $scope.heatmapSize - 1; }
+
+    var clusterTotal = 0;
+    var clusterMarks = [];
+    var callb = function (item, pos) {
+      return clusterKeys.indexOf(item) === pos;
+    };
+    for (var r = clusterStartRow; r <= clusterEndRow; r++) {
+      for (var c = clusterStartCol; c <= clusterEndCol; c++) {
+        try {
+          clusterKeys = clusterKeys.concat($scope.output.keywords[r][c]);
+          clusterKeys = clusterKeys.filter(callb);
+        }
+        catch (e) { }
+      }
+    }
+
+    return clusterKeys;
+  };
+
+  $scope.suggestionAnalysis = function () {
+    //$scope.suggestionsLoading = true;
+    var startRow = $scope.suggestionClickedStart.split('-')[0];
+    var startCol = $scope.suggestionClickedStart.split('-')[1];
+    //$scope.suggestionMentionLess = $scope.findClusterKeywords(startRow, startCol);
+    $scope.suggestionMentionLess = $scope.output.keywords[startRow][startCol];
+    var endRow = $scope.suggestionClickedEnd.split('-')[0];
+    var endCol = $scope.suggestionClickedEnd.split('-')[1];
+    $scope.suggestionMentionMore = $scope.findClusterKeywords(endRow, endCol);
+    for (var i = 0; i < $scope.suggestionMentionLess.length; i++) {
+      var index = $scope.suggestionMentionMore.indexOf($scope.suggestionMentionLess[i]);
+      if (index) { $scope.suggestionMentionMore.splice(index, 1); }
+    }
   };
 
   $scope.openCloseView = function (view) {
@@ -124,7 +200,8 @@ angular.module('semavisApp').controller('MainCtrl', function ($rootScope, $scope
     //for (var i = 0; i < slices.data.length; i++) {
     //  var slicedText = slices.data[i].text;
     api.extractKeywords($scope.input.corpus).then(function (res) {
-      $scope.output.relatedKeywords = $scope.output.relatedKeywords.concat(res.data);
+      //$scope.output.relatedKeywords = $scope.output.relatedKeywords.concat(res.data);
+      $scope.output.relatedKeywords = res.data;
       $scope.highlightRegex();
       $scope.processRelatedKeywords(res.data);
       $rootScope.loading = false;
@@ -177,34 +254,36 @@ angular.module('semavisApp').controller('MainCtrl', function ($rootScope, $scope
       val: clusterTotal
     });
 
+    $scope.clustersSortAndTrim();
+
+  };
+
+  $scope.isClusterMarked = function (r, c) {
+    //if ($scope.output.clusterMarks.length > 0){ console.log(r, c, $scope.output.clusterMarks); }
+    return $scope.output.clusterMarks.map(function (i) { return i.key; }).indexOf(r + '-' + c) !== -1;
+  };
+
+  $scope.clustersSortAndTrim = function () {
     $scope.output.clusterMarks.sort(function (a, b) {
       if (a.val < b.val) { return 1; }
       if (a.val > b.val) { return -1; }
       return 0;
     });
-
     if ($scope.output.clusterMarks.length > $scope.clusterQuantity) {
       $scope.output.clusterMarks.length = $scope.clusterQuantity;
     }
-
-    //if (clusterTotal > $scope.clusterMax) {
-    //  $scope.clusterMax = clusterTotal;
-    //  $scope.output.clusterMarks.push(row + '-' + col);
-    //}
   };
 
-  $scope.isClusterMarked = function (r, c) {
-    //if ($scope.output.clusterMarks.length > 0){ console.log(r, c, $scope.output.clusterMarks); }
-    return $scope.output.clusterMarks.map(function(i){ return i.key; }).indexOf(r + '-' + c) !== -1;
-  };
-
-  $scope.insertDataToHeatmap = function (dataArr) {
+  $scope.insertDataToHeatmap = function (term, dataArr) {
     if (!$scope.output.heatmap) { $scope.output.heatmap = []; }
     for (var i = 0; i < dataArr.length; i++) {
       var index = dataArr[i];
       var row = index % $scope.heatmapSize;
       var col = (index - row) / $scope.heatmapSize;
       $scope.output.heatmap[row][col] += 1;
+      if (!$scope.output.keywords[row]) { $scope.output.keywords[row] = []; }
+      if (!$scope.output.keywords[row][col]) { $scope.output.keywords[row][col] = []; }
+      $scope.output.keywords[row][col].push(term);
       // find clusters
       $scope.findCluster(row, col);
       // find max boundry
@@ -216,7 +295,7 @@ angular.module('semavisApp').controller('MainCtrl', function ($rootScope, $scope
     if (!$scope.output.fingerprints) { $scope.output.fingerprints = []; }
     api.getFingerprints(term).then(function (data) {
       $scope.output.fingerprints.push(data.data[0].fingerprint.positions);
-      $scope.insertDataToHeatmap(data.data[0].fingerprint.positions);
+      $scope.insertDataToHeatmap(term, data.data[0].fingerprint.positions);
     }, function () {
       swal('Oops...', 'Something went wrong while generating the fingerprint', 'error');
     });
@@ -225,7 +304,7 @@ angular.module('semavisApp').controller('MainCtrl', function ($rootScope, $scope
   $scope.processRelatedKeywords = function (keys) {
     for (var i = 0; i < keys.length; i++) {
       $scope.getFingerPrint(keys[i]);
-      $scope.getSuggestedKeywords(keys[i]);
+      //$scope.getSuggestedKeywords(keys[i]);
     }
   };
 
